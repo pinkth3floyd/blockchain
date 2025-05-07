66a93ef2e329c0a209202f78a9e697f3e891aa16
pinkth3floyd <prakash@zeptosky.com>
2025-05-07 18:56:12 +0545

    tooltip integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "./core/ui/elements/tooltip";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
     
     <html lang="en">
        <QueryClientProvider client={queryClient}>
+       <TooltipProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      </TooltipProvider>
       </QueryClientProvider>
     </html>
   );
