87d514d3086895a64e62362e071a80c1ef4e3b4a
pinkth3floyd <prakash@zeptosky.com>
2025-05-07 20:09:32 +0545

    ToolTip provider build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,9 +37,9 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
              {/* <QueryClientProvider client={queryClient}> */}
-             {/* <TooltipProvider> */}
+             <TooltipProvider>
             {children}
-            {/* </TooltipProvider> */}
+            </TooltipProvider>
             {/* </QueryClientProvider> */}
           </body>
        
